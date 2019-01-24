f795045b4b5fbde3ce6630daee93838dde265b57
hans couder <hans.couder@cybecube.com>
2019-01-24 08:46:04 +0100

    ttr

--- pydev/cybecube/database/__init__.py
+++ pydev/cybecube/database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///C:\\Users\\couder\\Projects\\pydev\\test.db')
+engine = create_engine('sqlite:///C:\\Users\\HansCouder\\databases>\\test.db')
 
 Base = declarative_base()
 
